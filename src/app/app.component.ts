f896f4e627595f76de81503bcc954357d290b6ce
Alex <millan2993@gmail.com>
2018-10-18 01:15:05 -0500

    Modificación parcial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amillan';
+  updating:number = 0;
+
+  employees = [
+    {
+      'first_name': 'Carlos',
+      'last_name' : 'Perez'
+    },
+    {
+      'first_name': 'Juan',
+      'last_name' : 'Gómez'
+    }
+  ];
+
+  model:any = {};
+
+
+  save(): void {
+
+    if (this.updating == 0) {
+      this.employees.push(this.model);
+    }
+
+    this.updating = 0;
+    this.model = {};
+  }
+
+  edit(i):void {
+    this.model = this.employees[i];
+    this.updating = i+1;
+  }
+
+
+  delete(i):void {
+    this.employees.splice(i, 1);
+  }
+
 }
